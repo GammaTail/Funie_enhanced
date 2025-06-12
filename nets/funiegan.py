32b1f93152e8910b63b2f3c1c527670c401852d8
Sanidhya Agrawal <sanidhyaagrawal963@gmail.com>
2025-06-12 14:05:51 +0530

    Funie created

---
+++ nets/funiegan.py
@@ -0,0 +1,125 @@
+"""
+ > Network architecture of FUnIE-GAN model
+   * Paper: arxiv.org/pdf/1903.09766.pdf
+ > Maintainer: https://github.com/xahidbuffon
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+import torch
+import torch.nn as nn
+
+# Residual Block for Bottleneck
+class ResBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.InstanceNorm2d(channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.InstanceNorm2d(channels),
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+class UNetDown(nn.Module):
+    def __init__(self, in_size, out_size, bn=True):
+        super(UNetDown, self).__init__()
+        layers = [nn.Conv2d(in_size, out_size, 4, 2, 1, bias=False)]
+        if bn:
+            layers.append(nn.InstanceNorm2d(out_size))  # Changed from BatchNorm2d
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
+class UNetUp(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(UNetUp, self).__init__()
+        layers = [
+            nn.ConvTranspose2d(in_size, out_size, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(out_size),  # Changed from BatchNorm2d
+            nn.ReLU(inplace=True),
+        ]
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x, skip_input):
+        x = self.model(x)
+        x = torch.cat((x, skip_input), 1)
+        return x
+
+class GeneratorFunieGAN(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3):
+        super(GeneratorFunieGAN, self).__init__()
+        # encoding layers
+        self.down1 = UNetDown(in_channels, 32, bn=False)
+        self.down2 = UNetDown(32, 128)
+        self.down3 = UNetDown(128, 256)
+        self.down4 = UNetDown(256, 256)
+        self.down5 = UNetDown(256, 256, bn=False)
+
+        # bottleneck residual block
+        self.resblock = ResBlock(256)
+
+        # decoding layers
+        self.up1 = UNetUp(256, 256)
+        self.up2 = UNetUp(512, 256)
+        self.up3 = UNetUp(512, 128)
+        self.up4 = UNetUp(256, 32)
+        self.final = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.ZeroPad2d((1, 0, 1, 0)),
+            nn.Conv2d(64, out_channels, 4, padding=1),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        d1 = self.down1(x)
+        d2 = self.down2(d1)
+        d3 = self.down3(d2)
+        d4 = self.down4(d3)
+        d5 = self.down5(d4)
+
+        # Pass through residual block
+        bottleneck = self.resblock(d5)
+
+        u1 = self.up1(bottleneck, d4)
+        u2 = self.up2(u1, d3)
+        u3 = self.up3(u2, d2)
+        u45 = self.up4(u3, d1)
+        return self.final(u45)
+
+
+
+import torch
+import torch.nn as nn
+
+class DiscriminatorFunieGAN(nn.Module):
+    """ A 4-layer PatchGAN discriminator with InstanceNorm """
+    def __init__(self, in_channels=3):
+        super(DiscriminatorFunieGAN, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+            if bn:
+                layers.append(nn.InstanceNorm2d(out_filters))  # Changed from BatchNorm2d
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *discriminator_block(in_channels * 2, 32, bn=False),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
+            *discriminator_block(128, 256),
+            nn.ZeroPad2d((1, 0, 1, 0)),
+            nn.Conv2d(256, 1, 4, padding=1, bias=False)
+        )
+
+    def forward(self, img_A, img_B):
+        img_input = torch.cat((img_A, img_B), 1)
+        return self.model(img_input)
